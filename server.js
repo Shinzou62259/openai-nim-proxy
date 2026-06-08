d8bc84d716e602691d8f539a6611a4413cfaefa9
Shinzou62259 <someone62257@gmail.com>
2026-06-08 13:52:43 +0530

    Update server.js

--- server.js
+++ server.js
@@ -33,7 +33,6 @@ const MODEL_MAPPING = {
   'gpt-4-turbo': 'moonshotai/kimi-k2-instruct-0905',
   'gpt-4o': 'deepseek-ai/deepseek-v3.1-terminus',
   'claude-3-opus': 'openai/gpt-oss-120b',
-  'claude-3-sonnet': 'openai/gpt-oss-20b',
   'gemini-pro': 'qwen/qwen3-next-80b-a3b-thinking',
   'deepseek': 'deepseek-ai/deepseek-v4-flash'
 };
